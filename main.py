44ed47f3ed36c55e15906f41b5e5bb98f98284df
nwwaiyankyaw87-hue <nw.waiyankyaw87@gmail.com>
2026-05-25 19:23:19 +0630

    Update main.py

--- main.py
+++ main.py
@@ -128,7 +128,7 @@ async def handle_text(update: Update, context: ContextTypes.DEFAULT_TYPE):
         search_norm = item["search"]
 
         if q and q in search_norm:
-        key = item["brand"] + item["model"] + item["code"]
+            key = item["brand"] + item["model"] + item["code"]
 
             if key not in seen:
                 matches.append(item)
