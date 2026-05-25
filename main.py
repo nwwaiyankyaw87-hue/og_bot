d8f6466c7099d6891e90216190313961ef6dc06e
nwwaiyankyaw87-hue <nw.waiyankyaw87@gmail.com>
2026-05-25 16:05:01 +0630

    Update main.py

--- main.py
+++ main.py
@@ -74,6 +74,10 @@ def split_models_with_brand(model_text):
             # ONEPLUS Models
             elif model.upper().startswith(("NORD", "ACE")):
                 current_brand = "ONEPLUS"
+
+            # MOTOROLA Models
+            elif model.upper().startswith(("G", "E", "EDGE", "MOTO")):
+                current_brand = "MOTOROLA"
             if model:
                 result.append((current_brand, model))
 
