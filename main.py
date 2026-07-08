91ab9b97c0e55f6d4d06ef4dee7bf4c8909f97cb
nwwaiyankyaw87-hue <nw.waiyankyaw87@gmail.com>
2026-07-08 12:59:55 +0630

    Update main.py

--- main.py
+++ main.py
@@ -237,7 +237,6 @@ app = ApplicationBuilder().token(BOT_TOKEN).build()
 
 app.add_handler(CommandHandler("start", start_command))
 app.add_handler(CommandHandler("reset", reset_user))
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_text))
 app.add_handler(CallbackQueryHandler(handle_button))
 
 print("Bot started...")
