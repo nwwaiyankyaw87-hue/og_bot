4ea28da0259c289c033bf1a6c200310dcdc8e4ac
nwwaiyankyaw87-hue <nw.waiyankyaw87@gmail.com>
2026-07-08 11:13:33 +0630

    Update main.py

--- main.py
+++ main.py
@@ -204,7 +204,7 @@ async def handle_text(update: Update, context: ContextTypes.DEFAULT_TYPE):
             else:
                 await update.message.reply_text("⚠️ ကျေးဇူးပြု၍ ပြထားသည့်အတိုင်း **[ ဆိုင်အမည် - ဖုန်းနံပါတ် ]** ပုံစံအတိုင်း သေချာစွာ ရိုက်ထည့်ပေးပါ။")
             return
-            else:
+                else:
                 await update.message.reply_text("⛔️ သင့်အား ဗော့တ်အသုံးပြုခွင့် ပိတ်ပင်ထားပါသည်။")
             return
             except Exception: pass
